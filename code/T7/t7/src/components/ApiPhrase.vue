f3d874a5a4d7fb92ebd5926ffc772c180929b928
organic-waste <15928448056@qq.com>
2024-10-12 21:45:34 +0800

    T7

--- code/T7/t7/src/components/ApiPhrase.vue
+++ code/T7/t7/src/components/ApiPhrase.vue
@@ -1,47 +1,64 @@
 <template>
 <div class="box">
-    <div class="pbox">
-        <div class="phrase">{{sentence}}</div>
-        <img src="..\assets\good.svg" alt="upvote" class="upvote" @click="upnum++"/>
-        <span>{{ '点赞数：'+upnum }}</span>
-    </div>
+    <template v-for="phrase in sortedPhrases" :key="phrase.upnum">
+      <div class="pbox">
+        <div class="phrase">{{phrase.sentence||0}}</div>
+        <img src="../assets/good.svg" alt="upvote" class="upvote" @click="phrase.upnum++"/>
+        <span>{{ '点赞数：'+phrase.upnum||0 }}</span>
+      </div>
+    </template>
 </div>
 </template>
 
 <script setup>
-// import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted,computed} from 'vue';
 import axios from 'axios';
 
-//点赞功能
-var upnum=ref(0);
 
 //请求数据
-var sentence=ref("数据未能成功获得");
+var phrases=ref([]);
+class Sen {
+  constructor(sentence, upnum) {
+    this.sentence=sentence;
+    this.upnum = upnum;
+  }
+}
 onMounted(() => {
-  axios.get('https://tenapi.cn/v2/yiyan').then(
-    res => {
-      if (res.status === 200 && res.data) {
-        sentence.value=`${res.data}`;
-        // sentence.value ='请求成功';
-      } else {
-        console.error('API 返回数据失败:', res);
-        sentence.value ='请求失败1';
-      }
-    },
-    err => {
-      console.error('请求失败:', err.message);
-      sentence.value ='请求失败2';
-    }
-  );
+  const promises = []; 
+  for (let i = 0; i < 10; i++) {
+    promises.push(
+      axios.get('https://tenapi.cn/v2/yiyan').then(res => {
+        if (res.status === 200 && res.data) {
+          const sen = new Sen(res.data, 0);
+          phrases.value.push(sen); // 将新实例推入数组而不是直接赋值
+          sortedPhrases;
+        } else {
+          console.error('API 返回数据失败:', res);
+        }
+      }).catch(err => {
+        console.error('请求失败:', err.message);
+      })
+    );
+  }
+
+  Promise.all(promises).then(() => {
+    console.log('所有请求完成');
+  });
 });
+
+//排序数组
+const sortedPhrases = computed(() => {
+  return [...phrases.value].sort((a, b) => b.upnum - a.upnum);
+});
+
 </script>
+
 
 <style>
 .box{
     display: flex;
-    align-items: center;  
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     padding: 10px; 
     border: 2px solid rgb(130, 110, 101);
     background-color: rgb(249, 246, 246);
@@ -51,13 +68,12 @@ onMounted(() => {
 
 .pbox {
   display: flex;
-  align-items: center;  
   justify-content: center;
+  vertical-align: middle;
   padding: 10px; 
   border: 2px solid rgb(130, 110, 101);
   background-color: rgb(250, 239, 233);
   width: 700px;
-  justify-content: center;
 }
 
 .upvote {
